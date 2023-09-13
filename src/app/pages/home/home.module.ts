efcbb3ff613a1b88951072d78f15418467ed03e2
juanherazo <jhondavid751988@hotmail.com>
2023-09-13 17:32:27 -0500

    se agrega el proyecto

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  declarations: [HomePage],
+  providers: [RickAndMortyService]
+})
+export class HomePageModule {}
